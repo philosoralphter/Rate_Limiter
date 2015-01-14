5b04af036ae026d96f44dddd5f6741400cd7a3ce
RSamuel@ApolloII <theralphsamuel@gmail.com>
2015-01-14 04:24:20 -0800

    Implements and renames RateLimiter.authorizeRequestAsync(), and updates README.md.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ##API Rate Limiter
-This is an npm module that will
+This is an npm module that will allow you to instantiate a RateLimiter object in your server.  You pass the information object can be used to 
 
 ###Installation
 Place the APIRateLimiter folder into your project folder.
@@ -13,12 +13,21 @@ In your server or your API client, require rateLimiter.js and instantiate a rate
 
 ```javascript
 var RateLimiter = require('../path-to/rateLimiter.js');
-
-var rateLimiter = new RateLimiter(6379, localhost, {});
 ```
 
+####const. RateLimiter(redisPORT:<integer>, redisIP:<string>, redisOptions:<object>)
+Instantiate a new rateLimiter object.
+```
+var rateLimiter = new RateLimiter(6379, localhost, {});
+```
+Redis Options:
+The third parameter of the RateLimiter constructor takes a javascript object with the following notable properties:
 
-####`rateLimiter.authorizeRequest(APIname:<string>, usr:<string>)`
+`auth_pass : <string>`If your redis database is secured, place the password here.
+
+Other available properties can be found [here]:https://www.npmjs.com/package/redis in the API documentation of the npm module redis.
+
+####`rateLimiter.authorizeRequestAsync(APIname:<string>, usr:<string>)`
 
 This function takes the name of an API being limited, and optionally, a string associated with the usr making the request.
 
@@ -32,7 +41,7 @@ It then checks the status of the user-specific limits on the API and returns `fa
 
 
 ###Roadmap
-*Separate code into incoming and outgoing rate limiters.  Expose specialized objects.
+*Separate code into incoming and outgoing rate limiters.  Expose specialized objects.  Create express middlewre for incoming limiters.
 
 *Add ability to handle request queueing in OutgoingRateLimiter.
 
--- rateLimiter.js
+++ rateLimiter.js
@@ -1,7 +1,7 @@
 module.exports.RateLimiter = RateLimiter;
 
 
-function RateLimiter(redisPORT, redisIP, redisOptions) = {
+function RateLimiter(redisPORT, redisIP, redisOptions) {
   var redis = require('redis');
   var shouldAuthenticate = redisOptions.hasOwnProperty('password') ? true : false;
   var redisPassword = redisOptions.password || null;
@@ -9,13 +9,61 @@ function RateLimiter(redisPORT, redisIP, redisOptions) = {
   //--------------
   //Public Methods
   //---------------
-  this.authorizeRequest = function(APIname, usr, callback){
-    var redisClient = redis.createClient();
+  this.authorizeRequestAsync = function(APIname, usr, mainCallback){
+    var allClear;
+    var redisClient = redis.createClient(redisPORT, redisIP, redisOptions);
+    
+    rediClient.HGETALL(APIname + ' Settings', function(err, reply){
+      if (err){console.log(err);}
+      var APIOptions = reply;
 
+      if (APIOptions.globalLimit && APIOptions.perUserLimit){
+        checkGlobalLimit(APIname, function(err, response){
+          if (err){console.log(err);}
+          if (response != true){
+            console.log('Global Limit Exceeded for API: ', APIname);
+            mainCallback(false);
+          }else{
+            checkPerUserLimit(APIname, usr, function(err, response){
+              if (err){console.log(err);}
+              if (response != true){
+                console.log('Per User Limit Exceeded for API: ', APIname, 'and user: ', usr);
+                mainCallback(false);
+              }else{
+                mainCallback(true);
+              }
+            });
+          }
+        });
+      }else if (APIOptions.globalLimit){
+        checkGlobalLimit(APIname, function(err, response){
+          if (err){console.log(err);}
+          if (response != true){
+            console.log('Global Limit Exceeded for API: ', APIname);
+            mainCallback(false);
+          }else{
+            mainCallback(true);
+          }
+        });
+      }else if (APIOptions.perUserLimit){
+        checkPerUserLimit(APIname, usr, function(err, response){
+          if (err){console.log(err);}
+          if (response != true){
+            console.log('Per User Limit Exceeded for API: ', APIname, 'and user: ', usr);
+            mainCallback(false);
+          }else{
+            mainCallback(true);
+          }
+        });
+      }else{
+        console.log('No limits set for API: ', APIname);
+        mainCallback(true);
+      }
+    });
   }
 
   this.setPerUserLimit = function(APIname, limit, timeWindow){
-    var redisClient = redis.createClient();
+    var redisClient = redis.createClient(redisPORT, redisIP, redisOptions);
     var apiOptions = {
       "APIOptionsHashTable": APIname + ' Settings',
       "perUserLimit" : limit,
@@ -28,30 +76,23 @@ function RateLimiter(redisPORT, redisIP, redisOptions) = {
   }
 
   this.setGlobalLimit = function(APIname, limit, timeWindow){
-    var redisClient = redis.createClient();
+    var redisClient = redis.createClient(redisPORT, redisIP, redisOptions);
     var apiOptions = {
       "APIOptionsHashTable": APIname + ' Settings',
       "globalLimit" : limit,
       "globalLimitTimeWindow" : timeWindow
     }
-    //store each api's info in a hash table in redis named APIName Settings
+    //store each api's info in a hash table in redis named APIname Settings
     redisClient.HMSET(apiOptions.APIOptionsHashTable, 'globalLimit', apiOptions.globalLimit, 'globalLimitTimeWindow', apiOptions.timeWindow);
   }
 
   //Private functions
-  function checkGlobalLimit(APIname){
+  function checkGlobalLimit(APIname, callback){
+    var redisClient = redis.createClient(redisPORT, redisIP, redisOptions);
 
   }
 
-  function checkUsrLimit(APIname, usr){
+  function checkUsrLimit(APIname, usr, callback){
 
-  }
-
-  function authorizeRedisConnection(client, callback){
-    if (shouldAuthenticate){
-      client.auth(redisPassword, callback);
-    }else{
-      callback();
-    }
   }
 }
